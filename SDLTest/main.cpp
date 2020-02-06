631329753ad6d5d538d3db49309c0ac3d4f8f3c9
guilhermetoda <guilhermetoda@gmail.com>
2020-02-06 11:31:20 -0300

    Initial Commit

---
+++ SDLTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDLTest
+//
+//  Created by Guilherme Toda on 06/02/20.
+//  Copyright © 2020 Guilherme Toda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
